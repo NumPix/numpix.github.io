7dfa4f0f054b45eff118df9f7b43e74f46e954b6
NumPix <lecpix@vk.com>
2025-08-27 04:27:58 +0300

    fixed THREE imports

--- Vertex.js
+++ Vertex.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.162.0/build/three.module.js'
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.179.1/build/three.module.js'
 
 const k = 0.3;
 const restLength = 1;
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.162.0/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.179.1/build/three.module.js';
 import * as States from './States.js';
 import { Graph } from './Graph.js';
 import { PhysicsEngine } from './Physics.js';
